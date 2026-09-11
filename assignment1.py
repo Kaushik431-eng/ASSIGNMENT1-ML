509470bab8c51ddc837193c06a23e0f47c316c7e
Kaushik Chavda <kaushikchavda919@gmail.com>
2026-09-12 02:12:07 +0530

    file uploaded

---
+++ assignment1.py
@@ -0,0 +1,34 @@
+# question 1
+
+my_integer = 50
+my_float = 10.10
+my_complex = 3 + 4j
+my_string = "This is my first assignment in machine learning"
+my_boolean = True
+
+print("Integer:", my_integer, "| Type:", type(my_integer))
+print("Float:", my_float, "| Type:", type(my_float))
+print("Complex:", my_complex, "| Type:", type(my_complex))
+print("String:", my_string, "| Type:", type(my_string))
+print("Boolean:", my_boolean, "| Type:", type(my_boolean))
+
+# question 2
+
+my_string = "this is my first python llm programe"
+
+print ("length:" , len(my_string))
+
+print ("uppercase:",my_string.upper())
+
+modified_string = my_string.replace("llm" ,"ml")
+print("modified string:",modified_string)
+
+# question 3
+
+my_list= [10,20,"kaushik",12.12]
+
+print("first element:",my_list[1])
+print("last element:",my_list[-1])
+print("second to three element:", my_list[1:2])
+
+
